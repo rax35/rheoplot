1c16b4c2d241a1e8107d51d0327c67b2463721ad
Ved Prajapati <104439483+rax35@users.noreply.github.com>
2026-05-29 15:23:45 +0530

    License

--- src/fminsearch.ts
+++ src/fminsearch.ts
@@ -1,6 +1,42 @@
-// @ts-nocheck
+/*
+https://github.com/jonasalmeida/fminsearch
 
-export const fminsearch=function(fun,Parm0,x,y,Opt){// fun = function(x,Parm)
+MIT License
+
+Copyright (c) Jonas Almeida.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+interface Opts {
+	maxIter?: number,
+	step?: number[],
+	objFun?: any
+}
+
+export function fminsearch(
+	fun: (x: number[], Parm: number[]) => number[], Parm0: number[],
+	x: number[],
+	y: number[],
+	Opt?: Opts
+) {
+	// fun = function(x,Parm)
 	// example
 	//
 	// x = [32,37,42,47,52,57,62,67,72,77,82,87,92];y=[749,1525,1947,2201,2380,2537,2671,2758,2803,2943,3007,2979,2992]
@@ -13,36 +49,40 @@ export const fminsearch=function(fun,Parm0,x,y,Opt){// fun = function(x,Parm)
 	// Opt is an object will all other parameters, from the objective function (cost function), to the 
 	// number of iterations, initial step vector and the display switch, for example
 	// Parms=fminsearch(fun,[100,30,10,5000],x,y,{maxIter:10000,display:false})
-	
-	if(!Opt){Opt={}};
-	if(!Opt.maxIter){Opt.maxIter=1000};
-	if(!Opt.step){// initial step is 1/100 of initial value (remember not to use zero in Parm0)
-		Opt.step=Parm0.map(function(p){return p/100});
-		Opt.step=Opt.step.map(function(si){if(si==0){return 1}else{ return si}}); // convert null steps into 1's
+
+	if (!Opt) { Opt = {} };
+	if (!Opt.maxIter) { Opt.maxIter = 1000 };
+	if (!Opt.step) {// initial step is 1/100 of initial value (remember not to use zero in Parm0)
+		Opt.step = Parm0.map(function(p) { return p / 100 });
+		Opt.step = Opt.step.map(function(si) { if (si == 0) { return 1 } else { return si } }); // convert null steps into 1's
 	};
-	if(typeof(Opt.display)=='undefined'){Opt.display=true};
-	if(!Opt.objFun){Opt.objFun=function(y,yp){return y.map(function(yi,i){return Math.pow((yi-yp[i]),2)}).reduce(function(a,b){return a+b})}} //SSD
-	
-	var cloneVector=function(V){return V.map(function(v){return v})};
-	var ya,y0,yb,fP0,fP1;
-	var P0=cloneVector(Parm0),P1=cloneVector(Parm0);
+	if (!Opt.objFun) {
+		Opt.objFun = (y: number[], yp: number[]) => {
+			return y.map((yi, i) => {
+				return Math.pow((yi - yp[i]), 2)
+			}).reduce((a, b) => { return a + b })
+		} //SSD
+	}
+
+	var cloneVector = (V: number[]) => { return V.map((v) => { return v }) };
+	// var ya, y0, yb, fP0, fP1;
+	var P0 = cloneVector(Parm0), P1 = cloneVector(Parm0);
 	var n = P0.length;
-	var step=Opt.step;
-	var funParm=function(P: any){return Opt.objFun(y,fun(x,P))}//function (of Parameters) to minimize
+	var step = Opt.step;
+	var funParm = function(P: any) { return Opt.objFun(y, fun(x, P)) }//function (of Parameters) to minimize
 	// silly multi-univariate screening
-	for(var i=0;i<Opt.maxIter;i++){
-		for(var j=0;j<n;j++){ // take a step for each parameter
-			P1=cloneVector(P0);
-			P1[j]+=step[j];
-			if(funParm(P1)<funParm(P0)){ // if parm value going in the righ direction
-				step[j]=1.2*step[j]; // then go a little faster
-				P0=cloneVector(P1);
+	for (var i = 0; i < Opt.maxIter; i++) {
+		for (var j = 0; j < n; j++) { // take a step for each parameter
+			P1 = cloneVector(P0);
+			P1[j] += step[j];
+			if (funParm(P1) < funParm(P0)) { // if parm value going in the righ direction
+				step[j] = 1.2 * step[j]; // then go a little faster
+				P0 = cloneVector(P1);
 			}
-			else{
-				step[j]=-(0.5*step[j]); // otherwiese reverse and go slower
-			}	
+			else {
+				step[j] = -(0.5 * step[j]); // otherwiese reverse and go slower
+			}
 		}
-		if(Opt.display){if(i>(Opt.maxIter-10)){console.log(i+1,funParm(P0),P0)}}
 	}
 	return P0
 };
